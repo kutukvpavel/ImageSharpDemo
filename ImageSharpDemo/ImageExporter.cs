f57dc7d73880a14e21648b4f70873e7c6061c1ca
kutukvpavel <41666738+kutukvpavel@users.noreply.github.com>
2021-11-22 21:33:08 +0300

    Добавьте файлы проекта.

---
+++ ImageSharpDemo/ImageExporter.cs
@@ -0,0 +1,36 @@
+﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageSharpDemo
+{
+    public static class ImageExporter
+    {
+        public static Font TagFont { get; set; } = new Font(SystemFonts.Get("Arial"), 200);
+
+        public static void DrawRect(Rectangle bounds, PlottableRect pRect, float tagRotation)
+        {
+            RectangleF r = pRect.Rect;
+            var textPoint = RectangleF.Center(r);
+            var tMeasure = TextMeasurer.Measure(pRect.Tag, new TextOptions(TagFont));
+            float tLenOffset = -tMeasure.Width / 2;
+            float tHOffset = -tMeasure.Height / 2;
+            textPoint.Offset(tHOffset, -tLenOffset); //Right for vertical orientation, doesn't matter since the example uses a square
+            var matrix = Matrix3x2Extensions.CreateRotationDegrees(tagRotation, textPoint);
+
+            using Image<Rgba32> image = new Image<Rgba32>(bounds.Width, bounds.Height, Color.Black);
+
+            image.Mutate(x => x.Fill(pRect.Color, r));
+            image.Mutate(x =>
+            {
+                var rdo = new DrawingOptions { Transform = matrix };
+                x.DrawText(rdo, pRect.Tag, TagFont, Color.White, textPoint);
+                x.Draw(rdo, Color.White, 5.0f, new RectangleF(textPoint, new SizeF(tMeasure.Width, tMeasure.Height)));
+            });
+
+            image.SaveAsPng("result.png");
+        }
+    }
+}
---
+++ ImageSharpDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using SixLabors.ImageSharp;
+using System;
+
+namespace ImageSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new PlottableRect(new Rectangle(0, 0, 1000, 1000))
+            {
+                Tag = "0,t=0",
+                Color = Color.Blue
+            };
+            ImageExporter.DrawRect(new Rectangle(0, 0, 1200, 1200), example, -90);
+        }
+    }
+}
